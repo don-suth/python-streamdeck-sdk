16897a73e797e6454b65666a2f6b79bf9db74c67
Donsuth <donald@sutherland.id.au>
2023-10-04 20:09:39 +0800

    Remove unneeded import

--- src/streamdeck_sdk/sdk.py
+++ src/streamdeck_sdk/sdk.py
@@ -3,7 +3,7 @@ import asyncio
 import json
 import logging
 from pathlib import Path
-from typing import Optional, List, Dict, Callable, Awaitable, Coroutine
+from typing import Optional, List, Dict, Callable, Coroutine
 
 import pydantic
 import websockets
